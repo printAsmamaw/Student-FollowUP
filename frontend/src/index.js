6885e2e4f410bfc440b8b05875e750025e3796e3
Hamdala Tamirat <98209561+hamdegithub@users.noreply.github.com>
2024-07-15 10:04:28 +0300

    frist time push checking

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import "./asset/css/feather.css";
+import "./asset/css/style.css";
+import "./asset/css/bootstrap.min.css";
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
